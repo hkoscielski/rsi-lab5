79d876ab0ffaedf541311e09002e3712920c5682
Kori08 <Kori08@users.noreply.github.com>
2018-06-11 20:46:02 +0200

    Dokumentacja #1

--- MojKlientWindow/JsonParser.cs
+++ MojKlientWindow/JsonParser.cs
@@ -9,8 +9,18 @@ using System.Threading.Tasks;
 
 namespace MojKlientWindow
 {
+    /// <summary>
+    /// Pomocznicza klasa statyczna. Zapewnia serializację i deserializację danych w formacie JSON.
+    /// Zapewnia metody konwertujące między: obiekt w formacie JSON, a obiektem Studenta lub Listą Studentów. W obie strony.
+    /// Autor: 228172. Hubert Kościelski.
+    /// </summary>
     static class JsonParser
     {
+        /// <summary>
+        /// Metoda statyczna zapewniająca serializację obiektu Studenta do obiektu w formacie JSON.
+        /// </summary>
+        /// <param name="student">Student Obiekt Studenta, który powinien zostać zwrócony w formacie JSON.</param>
+        /// <returns>string Napis będący obiektem w formacie JSON powstały z serializacji obiektu Studenta.</returns>
         public static string WriteFromObject(Student student)
         {
             MemoryStream ms = new MemoryStream();
@@ -22,6 +32,11 @@ namespace MojKlientWindow
             return Encoding.UTF8.GetString(json, 0, json.Length);
         }
 
+        /// <summary>
+        /// Metoda statyczna zapewniająca deserializację obiektu w formacie JSON do obiektu Studenta.
+        /// </summary>
+        /// <param name="json">string Napis będący obiektem Studenta w formacie JSON.</param>
+        /// <returns>Student Obiekt Studenta powstały wskutek deserializacji obiektu JSON.</returns>
         public static Student ReadToObject(string json)
         {
             Student deserializedStudent = new Student();
@@ -33,6 +48,11 @@ namespace MojKlientWindow
             return deserializedStudent;
         }
 
+        /// <summary>
+        /// Metoda statyczna zapewniająca deserializację listy obiektów w formacie JSON do Listy Studentów.
+        /// </summary>
+        /// <param name="json">string Napis będący listą obiektów (Studentów) w formacie JSON.</param>
+        /// <returns>Lista Studentów Lista powstała wskutek deserializacji obiektu JSON.</returns>
         public static List<Student> ReadToListOfObjects(string json)
         {
             List<Student> deserializedList = new List<Student>();
--- MojKlientWindow/Program.cs
+++ MojKlientWindow/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/// <summary>
+/// Przestrzeń nazw aplikacji klienckiej z interfejsem graficznym. Zawiera implementację aplikacji klienckiej wraz zapewnieniem jej łączności z serwisem.
+/// Autor: 228172. Hubert Kościelski.
+/// </summary>
 namespace MojKlientWindow
 {
+    /// <summary>
+    /// Główna klasa aplikacji klienckiej. To ona jest wywoływana jako pierwsza. Wywołuje ona aplikację okienkową jako klasę Form1, która zapewnia interfejs graficzny klienta.
+    /// Autor: 228172. Hubert Kościelski.
+    /// </summary>
     static class Program
     {
         /// <summary>
-        /// Główny punkt wejścia dla aplikacji.
+        /// Główny punkt wejścia dla aplikacji. Metoda wywołuje aplikację okienkową będącą klasą Form1, która zapewnia interfejs graficzny klienta.
         /// </summary>
         [STAThread]
         static void Main()
